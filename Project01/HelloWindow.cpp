690acb58f805a4677b5eaa5ed694b1d53a1c2a90
Pawn <hanxu19990207@gmail.com>
2024-09-05 13:21:13 +0800

    test

--- Project01/HelloWindow.cpp
+++ Project01/HelloWindow.cpp
@@ -27,7 +27,6 @@ using namespace std;
 
 
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
